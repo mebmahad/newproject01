9188839bd29fc213cfe7aed78e1cf1371baec111
mebmahad <167326398+mebmahad@users.noreply.github.com>
2024-10-26 08:38:25 +0000

    add procure id

--- src/main.jsx
+++ src/main.jsx
@@ -93,7 +93,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "/add-procure",
+        path: "/add-procure/:id",
         element: (
           <AuthLayout authentication>
             {""}

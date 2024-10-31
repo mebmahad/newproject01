e77db00c034f94798a3bab6ecb455c6050f350e2
mebmahad <167326398+mebmahad@users.noreply.github.com>
2024-10-31 04:57:23 +0000

    locationupdate18

--- src/appwrite/confi.js
+++ src/appwrite/confi.js
@@ -1,11 +1,11 @@
-import { databases } from "../appwrite/config"; // Import the databases object from your config
+import { Databases } from "appwrite"; // Import the databases object from your config
 
 const databaseId = conf.appwriteDatabaseId;
 const collectionId = conf.appwriteCollectionIdlocation;
 
 export const getLocationsByLocation = async () => {
     try {
-        const response = await databases.listDocuments(databaseId, collectionId);
+        const response = await Databases.listDocuments(databaseId, collectionId);
         // Extract only the `location` attribute from each document
         const locations = response.documents.map((doc) => doc.location);
         return locations;

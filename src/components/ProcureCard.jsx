21ffd36fc294b1a260798354d27711b224ee1b38
mebmahad <167326398+mebmahad@users.noreply.github.com>
2024-11-09 07:56:29 +0000

    procureupdate18

--- src/components/ProcureCard.jsx
+++ src/components/ProcureCard.jsx
@@ -1,27 +1,25 @@
-import React from 'react'; 
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ProcureCard({ $id, Items}) {
-
-  
+function ProcureCard({ id, items }) {
   return (
-    <Link to={`/procure/${$id}`}>
+    <Link to={`/procure/${id}`}>
       {/* Procurement Info */}
-      <div className="mb-8">
-                        <ul className="list-disc pl-5">
-                            {procure.items && procure.items.length > 0 ? (
-                                procure.items.map((item, index) => (
-                                    <li key={index} className="mb-2">
-                                        <span className="font-semibold">Item:</span> {item.Item}
-                                        <span className="ml-4 font-semibold">Quantity:</span> {item.Quantity}
-                                        <span className="ml-4 font-semibold">Budget:</span> {item.BudgetAmount}
-                                    </li>
-                                ))
-                            ) : (
-                                <li>No items specified</li>
-                            )}
-                        </ul>
-                    </div>
+      <div className="mb-8 bg-white shadow-md p-4 rounded">
+        <ul className="list-disc pl-5">
+          {items && items.length > 0 ? (
+            items.map((item, index) => (
+              <li key={index} className="mb-2">
+                <span className="font-semibold">Item:</span> {item.Item}
+                <span className="ml-4 font-semibold">Quantity:</span> {item.Quantity}
+                <span className="ml-4 font-semibold">Budget:</span> {item.BudgetAmount}
+              </li>
+            ))
+          ) : (
+            <li>No items specified</li>
+          )}
+        </ul>
+      </div>
     </Link>
   );
 }

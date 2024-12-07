f1122a2b94b0fbc442143c1eb794f73ca70167b9
mebmahad <meb.mahad@gmail.com>
2024-12-07 15:00:07 +0530

    404error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'custom-root',
+  build: {
+    outDir: 'custom-out-dir',
+  },
   plugins: [react()],
-  root: './custom-root',
 })
